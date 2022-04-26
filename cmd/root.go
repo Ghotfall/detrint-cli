0ac75ab7b9f1a60d13e9f35ebf1546d1707a8f29
Andrii Myrhorodskyi <mirgorodskijav@gmail.com>
2022-04-26 15:15:55 +0300

    Update long description

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,8 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "detrint-cli",
 	Short: "Detrint is like Ansible, only in Golang and with gRPC :)",
-	Long:  "Detrint is like Ansible, only in Golang and with gRPC :)",
+	Long: `Detrint is a configuration management tool developed using Golang & gRPC.
+The main focus of the tool is speed and usability for DevOps Engineers`,
 }
 
 func Execute() {
